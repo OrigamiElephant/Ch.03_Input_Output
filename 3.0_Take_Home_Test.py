fc449693cb1a42bb0d60d2a39852ec12806545b7
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2018-12-19 11:57:39 -0600

    A little more done with Chapter3

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -17,7 +17,6 @@ print (17//9) Prediction:1 Output: 1
 print (17%9) Prediction: 1.8 Output: 8
 '''
 
-
 '''
 4. Write a a program where a user enters a base and height and you print the area of a triangle.
 '''
@@ -31,9 +30,6 @@ print(a)
 '''
 A=May the Force be with you
 print(A)
-
-
-
 '''
 6. Write a line of code that will ask the user for the length of a square's
 side and then print the area of the square
@@ -41,12 +37,14 @@ side and then print the area of the square
 S=int(input("Squares Side Length:"))
 S_Area=S*S
 
-'''7. Ask a user for the length of radii of an ellipse and then print its area. 
+'''
+7. Ask a user for the length of radii of an ellipse and then print its area. 
 Area=pi*a*b where a and b are the lengths of the major radii.
 '''
-
-
-
+a=int(input("Ellipse Radii height:"))
+b=int(input("Ellipse Radii Width:"))
+pi=3.14
+Ellipse_Area=pi*a*b
 '''
 8. Ask a user for moles, volume and temperature of a gas and print out the pressure. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
 volume, and R is the gas constant 8.3144.
@@ -55,8 +53,8 @@ volume, and R is the gas constant 8.3144.
 '''
 9. Ask a user for an integer and then print the square root.
 '''
-
-
+a=int(input("Integer:"))
+print()
 '''
 10. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration
 and then print out the Force on one line and "Get it?" on the next.
