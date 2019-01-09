012fc1f01ddaae048cb7093693b883d5ba680020
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-01-09 12:14:54 -0600

    Finished Chapter 3 again,again

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -11,7 +11,7 @@ base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 14
 '''
 b1=int(input("Enter base 1 length:"))
-b2=int(input("Enter base 2 length"))
+b2=int(input("Enter base 2 length:"))
 h=int(input("Enter height:"))
 a=(b1+b2)/2*h
 print(a)
